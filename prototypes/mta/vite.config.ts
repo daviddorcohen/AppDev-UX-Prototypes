98e29eaf9fe363926d25ff570dd1e20771e73fee
yuvalgalanti <yuvalgalanti@users.noreply.github.com>
2026-04-21 14:17:33 +0300

    Fix require() error: force CJS deps through Rollup transform

--- prototypes/mta/vite.config.ts
+++ prototypes/mta/vite.config.ts
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/AppDev-UX-Prototypes/mta/',
+  optimizeDeps: {
+    include: ['@patternfly/quickstarts', 'marked'],
+  },
   build: {
     commonjsOptions: {
+      include: [/node_modules/],
       transformMixedEsModules: true,
     },
   },
